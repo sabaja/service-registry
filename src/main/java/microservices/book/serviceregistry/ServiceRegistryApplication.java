69d4ceec629fb9de0d1efd354ee174b32b21db1e
sabaja <jacoposabatini76@gmail.com>
2019-02-08 17:04:17 +0100

    Bug fixing - Service Registration failed

--- src/main/java/microservices/book/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/microservices/book/serviceregistry/ServiceRegistryApplication.java
@@ -1,15 +1,21 @@
 package microservices.book.serviceregistry;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegistryApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		ConfigurableApplicationContext context =	SpringApplication.run(ServiceRegistryApplication.class, args);
+		System.out.println("Hit enter to terminate");
+		System.in.read();
+		context.close();
 	}
 
 }
